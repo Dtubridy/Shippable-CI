11838724fbfa3128a8f18ee3e20f3524080f9d03
DTubridy <tubbs@live.ie>
2019-11-13 17:35:05 +0000

    added base tests

---
+++ src/test/java/nuigalway/app/ModulesTest.java
@@ -0,0 +1,36 @@
+package nuigalway.app;
+
+import junit.framework.TestCase;
+
+public class ModulesTest extends TestCase {
+
+    public void testTestGetName() {
+    }
+
+    public void testTestSetName() {
+    }
+
+    public void testGetId() {
+    }
+
+    public void testSetId() {
+    }
+
+    public void testGetStudents() {
+    }
+
+    public void testSetStudents() {
+    }
+
+    public void testGetCourses() {
+    }
+
+    public void testSetCourses() {
+    }
+
+    public void testAddCourse() {
+    }
+
+    public void testAddStudent() {
+    }
+}
