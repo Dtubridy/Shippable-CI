5206dca142f7bd521562fca17cdd128cfd235e26
DTubridy <tubbs@live.ie>
2019-11-13 19:23:58 +0000

    base test

---
+++ src/test/java/nuigalway/app/StudentsTest.java
@@ -0,0 +1,18 @@
+package nuigalway.app;
+
+import static org.junit.Assert.assertTrue;
+import org.joda.time.LocalDate;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class StudentsTest
+{
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
